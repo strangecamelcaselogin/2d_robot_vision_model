ba509243209426b0cb63b71de9ebd3708315067c
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2016-03-29 23:03:16 +0300

    update

--- figure.py
+++ figure.py
@@ -1,6 +1,8 @@
 from random import randint, random
 from math import cos, sin, pi
 
+import shelve
+
 from settings_storage import settings
 
 
@@ -9,19 +11,19 @@ class Figure:
     implement randoms figures on field
     реализует фигуры на поле
     '''
-    used_points = []
+    _used_points = []
 
-    def __init__(self, game, surface, vertices=None):
-        self.game = game
+    def __init__(self, id, pygame, surface, load=False):
+        self.id = id
+        self.pygame = pygame
         self.surface = surface
         self.width = 1
         self.color = settings.black
-        self.vertices = vertices
+        self.vertices = []  # vertices
 
-        Figure.used_points.append(settings.SPAWN_POINT)
+        Figure._used_points.append(settings.SPAWN_POINT)
 
-        if self.vertices is None:
-            self.vertices = []
+        if not load:  # if self.vertices is None:
             x, y = 0, 0
             while True:
                 collide = False
@@ -31,23 +33,23 @@ class Figure:
                 y = randint(settings.MAX_FIGURE_RAD,
                             settings.DISPLAY_RES[1] - settings.MAX_FIGURE_RAD)
 
-                if len(Figure.used_points) == 0:
-                    break
-                else:
-                    for used_x, used_y in Figure.used_points:
-                        if (x - used_x)**2 + (y - used_y)**2 < 4 * settings.MAX_FIGURE_RAD**2:
-                            collide = True
+                for used_x, used_y in Figure._used_points:
+                    if (x - used_x)**2 + (y - used_y)**2 < 4 * settings.MAX_FIGURE_RAD**2:
+                        collide = True
 
                 if not collide:
                     break
 
-            Figure.used_points.append((x, y))
+            Figure._used_points.append((x, y))
             self.__gen_figure((x, y))
+
+        else: # LOAD
+            pass
 
     def __gen_figure(self, base_point):
         shapes_count = randint(settings.MIN_SHAPES_COUNT, settings.MAX_SHAPES_COUNT)
 
-        fi = 0 # NEED MORE RANDOM
+        fi = 0  # NEED MORE RANDOM
         for shape_number in range(shapes_count):
             r = randint(settings.MIN_FIGURE_RAD, settings.MAX_FIGURE_RAD)
             x = int(base_point[0] + r * cos(fi))
@@ -58,14 +60,18 @@ class Figure:
             fi += 2 * pi/shapes_count
 
     def draw(self):
-        self.game.draw.polygon(self.surface, self.color, self.vertices, self.width)
+        self.pygame.draw.polygon(self.surface, self.color, self.vertices, self.width)
 
     def draw_circles(self):
-        for point in Figure.used_points:
-            self.game.draw.circle(self.surface, settings.red, point, settings.MAX_FIGURE_RAD, 1)
+        for point in Figure._used_points:
+            self.pygame.draw.circle(self.surface, settings.red, point, settings.MAX_FIGURE_RAD, 1)
 
     def save(self):
-        pass
+        with shelve.open(settings.SHELVE_FILE_NAME) as db:
+            db[str(self.id)] = self.vertices
+        db.close()
 
     def load(self):
-        pass
+        with shelve.open(settings.SHELVE_FILE_NAME) as db:
+            self.vertices = db[str(self.id)]
+        db.close()
